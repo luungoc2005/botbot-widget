c434c6e40d23d7bddd789d886262deeb3ccac1dd
luungoc2005 <luungoc2005@gmail.com>
2020-09-09 12:27:23 +0800

    missing customizations

--- App.jsx
+++ App.jsx
@@ -48,15 +48,64 @@ export class App extends Component {
     } = this.props;
     
     const showWelcomeMessage = Boolean(welcomeMessage)
-    const { title, message } = {...defaultWelcomeMessageProps, ...(welcomeMessage || {})};
+    const { buttonText, message } = {...defaultWelcomeMessageProps, ...(welcomeMessage || {})};
 
-    return <Fragment>
+    return <div>
       {showWelcomeMessage && <div 
         class={`notify-bubble ${showBubble && 'show'}`} 
         onclick={this.handleToggleChatWidget}
       >
-        <span>{title}</span>
-        <p>{message}</p>
+        <div
+          style={{
+            padding: '24px 82px 24px 10px',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <img
+            style={{
+              display: 'inline-block',
+              width: 32,
+              height: 32,
+            }}
+            src={require('./avatar20200810.png')}
+          />
+          <div
+            dangerouslySetInnerHTML={{ __html: message }}
+            style={{
+              marginLeft: 5,
+              padding: '8px 20px 8px 20px',
+              background: '#E5E5E5',
+              color: '#373737',
+              borderRadius: '24px 24px 24px 4px',
+              display: 'inline-block',
+              whiteSpace: 'initial',
+            }}
+          />
+        </div>
+        <div
+          style={{
+            background: '#F4F4F4',
+            padding: '8px 32px',
+          }}
+        >
+          <div style={{
+              padding: '18px 32px',
+              background: '#0058A0',
+              borderRadius: 30,
+              whiteSpace: 'nowrap',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <span style={{ margin: '5px 0px', color: 'white', fontSize: 16 }}>
+              {buttonText}
+            </span>
+            <svg style={{ marginLeft: 12 }} width="37" height="23" viewBox="0 0 37 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M11.4434 0.500571C11.4434 0.137743 11.8177 -0.104281 12.1485 0.0446109L35.8653 10.7171C36.2583 10.894 36.2583 11.4522 35.8653 11.6291L12.1485 22.3016C11.8177 22.4505 11.4434 22.2084 11.4434 21.8456V13.0807L31.7915 11.1731L11.4434 9.26546V0.500571Z" fill="white"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M6.40433 1.3645C5.5759 1.3645 4.90433 2.03607 4.90433 2.8645V3.13405C4.90433 3.96248 5.5759 4.63405 6.40433 4.63405H8.30865C9.13708 4.63405 9.80865 3.96248 9.80865 3.13405V2.8645C9.80865 2.03607 9.13708 1.3645 8.30865 1.3645H6.40433ZM0 11.0384C0 10.21 0.671573 9.53838 1.5 9.53838H8.30866C9.13708 9.53838 9.80865 10.21 9.80865 11.0384V11.3079C9.80865 12.1364 9.13708 12.8079 8.30865 12.8079H1.5C0.671573 12.8079 0 12.1364 0 11.3079V11.0384ZM4.90433 19.2123C4.90433 18.3838 5.5759 17.7123 6.40433 17.7123H8.30865C9.13708 17.7123 9.80865 18.3838 9.80865 19.2123V19.4818C9.80865 20.3102 9.13708 20.9818 8.30865 20.9818H6.40433C5.5759 20.9818 4.90433 20.3102 4.90433 19.4818V19.2123Z" fill="white"/>
+</svg>
+          </div>
+        </div>
       </div>}
       <div class={`chat-content ${showWidget && 'show'}`} aria-expanded={showWidget}>
         {iframeMounted && <iframe 
@@ -70,6 +119,10 @@ export class App extends Component {
             ...iframeStyle,
           }}
         />}
+        <button 
+          class="minimize-button"
+          onClick={this.handleToggleChatWidget}
+        />
       </div>
       <button 
         class={`msg-button ${showWidget && 'show'}`}
@@ -77,6 +130,6 @@ export class App extends Component {
       >
         <img src={buttonIcon} alt="" />
       </button>
-    </Fragment>
+    </div>
   }
 }
--- index.js
+++ index.js
@@ -2,112 +2,146 @@ import { render, h } from 'preact';
 import { App } from './App';
 
 let rendered = false;
+
 const defaultProps = {
   url: null,
   welcomeMessage: defaultWelcomeMessageProps,
-  buttonIcon: 'https://dashboard.botbot.ai/assets/d16f4ba438e1cab71d86520c31ea1a29.png',
+  buttonIcon: require('./chatbot.gif'),
   iframeStyle: {},
+  position: {
+    top: undefined,
+    left: undefined,
+    bottom: 20,
+    right: 50,
+  }
 }
+
 export const defaultWelcomeMessageProps = {
-  title: 'BotBot.AI',
-  message: '👋 Hi, let me know if I can help!',
-  delay: 2000,
+  buttonText: 'Gửi tin nhắn cho Sari',
+  message: 'Xin chào! Mình là Sari - trợ lý ảo Sacombank. Mình có thể giúp gì cho bạn',
+  delay: 500,
   speed: 180,
 }
+
 window.BotBot = {
   render: (props) => {
-    const { welcomeMessage, url } = {...defaultProps, ...props};
+    const { welcomeMessage, url, position } = {...defaultProps, ...props};
     const { speed } = welcomeMessage || defaultWelcomeMessageProps;
 
     if (!url) console.error('BotBot URL unspecified');
 
     const css = `
-    .botbot-chat-widget {
-      position: fixed;
-      bottom: 20px;
-      right: 20px;
-      z-index: 1000;
-    }
-    .botbot-chat-widget *, .botbot-chat-widget *::before, .botbot-chat-widget *::after {
-      box-sizing: border-box;
-    }
-    .botbot-chat-widget .chat-content.show {
-      display: block;
-    }
-    .botbot-chat-widget .msg-button.show {
-      background: #f5f5f5;
-    }
-    .botbot-chat-widget .msg-button {
-      width: 60px;
-      height: 60px;
-      border-radius: 50%;
-      display: block;
-      padding: 15px;
-      border: 0;
-      box-shadow: 0 4px 5px #ddd, 4px 0 5px #ddd;
-      cursor: pointer;
-      outline: none;
-    }
-    .botbot-chat-widget .msg-button img {
-      max-width: 100%;
-    }
-    .botbot-chat-widget .msg-button:hover {
-      opacity: 0.9;
-    }
-    .botbot-chat-widget .chat-content {
-      display: none;
-      position: absolute;
-      bottom: calc(100% + 20px);
-      right: 0;
-      box-shadow: 0 4px 5px #ddd;
-      background: white;
-      height: 520px;
-      width: 90vw;
-      max-width: 500px;
-    }
-    .botbot-chat-widget .notify-bubble {
-      opacity: 0;
-      pointer-events: none;
-      max-width: 500px;
-      min-width: 280px;
-      background-color: white;
-      box-shadow: 0 4px 5px #ddd;
-      border-radius: 10px;
-      position: absolute;
-      bottom: calc(100% + 23px);
-      right: 0;
-      padding: 20px;
-      font-family: sans-serif;
-      cursor: pointer;
-      user-select: none;
-      transform: translateY(40px);
-    }
-    .botbot-chat-widget .notify-bubble.show {
-      opacity: 100;
-      pointer-events: auto;
-      transition: opacity ${speed}ms ease-out, transform ${speed}ms ease-out;
-      transform: translateX(0);
-    }
-    .botbot-chat-widget .notify-bubble span {
-      color: #666;
-      font-size: .8em;
-    }
-    .botbot-chat-widget .notify-bubble p {
-      margin-top: 10px;
-    }
-    @media only screen and (max-width: 600px) {
-      .botbot-chat-widget .chat-content {
-          position: fixed;
-          top: 4px;
-          right: 4px;
-          bottom: 96px;
-          width: 98vw;
-          max-width: initial;
-          height: initial;
-          border-radius: 0px;
-          z-index: 200;
-      }
-    }
+.botbot-chat-widget {
+  position: fixed;
+  ${position.top ? "top: " + position.top + "px;" : ""}
+  ${position.left ? "left: " + position.left + "px;" : ""}
+  ${position.bottom ? "bottom: " + position.bottom + "px;" : ""}
+  ${position.right ? "right: " + position.right + "px;" : ""}
+  z-index: 1006;
+}
+.botbot-chat-widget *, .botbot-chat-widget *::before, .botbot-chat-widget *::after {
+  box-sizing: border-box;
+}
+.botbot-chat-widget .chat-content.show {
+  display: block;
+}
+.botbot-chat-widget .msg-button {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  overflow: visible;
+  display: block;
+  border: 0;
+  cursor: pointer;
+  outline: none;
+  background-color: transparent;
+}
+.botbot-chat-widget .msg-button img {
+  width: 100px;
+  height: 100px;
+}
+.botbot-chat-widget .msg-button:hover {
+  opacity: 0.9;
+}
+.botbot-chat-widget .chat-content {
+  display: none;
+  position: absolute;
+  bottom: 100%;
+  right: 0;
+  box-shadow: 0 1px 2px #ddd;
+  background: white;
+  height: 520px;
+  width: 360px;
+  max-height: calc(100vh - ${position.top || (position.bottom ? position.bottom + 100 : 200)}px - 50px);
+  border-radius: 15px;
+  overflow: hidden;
+}
+.botbot-chat-widget .chat-content .minimize-button {
+  position: absolute;
+  top: 28px;
+  right: 20px;
+  width: 15px;
+  height: 2px;
+  background: white;
+  border-radius: 1px;
+  opacity: 1;
+  cursor: pointer;
+  border: 0;
+  outline: none;
+}
+.botbot-chat-widget .chat-content .minimize-button:hover {
+  opacity: .5;
+}
+.botbot-chat-widget .notify-bubble {
+  border-radius: 15px;
+  opacity: 0;
+  pointer-events: none;
+  width: 360;
+  background-color: white;
+  box-shadow: 0 1px 2px #ddd;
+  position: absolute;
+  bottom: calc(100% + 23px);
+  right: 0;
+  cursor: pointer;
+  user-select: none;
+  transform: translateY(40px);
+  overflow: hidden;
+}
+.botbot-chat-widget .notify-bubble.show {
+  opacity: 100;
+  pointer-events: auto;
+  transition: opacity ${speed}ms ease-out, transform ${speed}ms ease-out;
+  transform: translateX(0);
+}
+.botbot-chat-widget .notify-bubble span {
+  color: #666;
+  font-size: .8em;
+}
+.botbot-chat-widget .notify-bubble p {
+  margin-top: 10px;
+}
+@media only screen and (max-width: 480px) {
+  .botbot-chat-widget {
+    bottom: 0px;
+    right: 16px;
+  }
+  .botbot-chat-widget .chat-content {
+    position: fixed;
+    top: 4px;
+    right: 4px;
+    bottom: ${position.top ? position.top + 8 : position.bottom ? position.bottom + 88 : 108}px;
+    width: 98vw;
+    max-width: 360px;
+    height: initial;
+    max-height: initial;
+    z-index: 200;
+  }
+  .botbot-chat-widget .chat-content iframe {
+    width: 100%;
+    height: 100%;
+    max-width: initial;
+  }
+}
     `
     
     if (!rendered) {
